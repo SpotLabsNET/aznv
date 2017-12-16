a8419393a6a23f1d755ea9a37592b3c1c48fdecd
CodeMaster <SpotLabsNET@users.noreply.github.com>
2017-12-16 15:51:24 +0000

    Update MinerConfigPrep.ps1

--- MinerConfigPrep.ps1
+++ MinerConfigPrep.ps1
@@ -1,3 +1,2 @@
 ﻿Install-PackageProvider -Force -Scope AllUsers -Name ChocolateyGet
 Install-Package -Force -Scope AllUsers -Name xPSDesiredStateConfiguration,xComputerManagement,xSystemSecurity,xCertificate,xNetworking,cWindowsOS,cChoco,cPowerPlan,xStorage,xTimeZone
-Rename-Computer -Force -Restart -NewName C404xLA
