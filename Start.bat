8752e098fa90a55f9c4d0e26b9877b41e00065a3
CodeMaster <SpotLabsNET@users.noreply.github.com>
2018-01-04 21:46:14 -0500

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -8,7 +8,7 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 
 rem set "command=& .\multipoolminer.ps1 -wallet 3KAQAwbnCAb2ZbUPVpmCsQYLv5fGj7UVDa -username OllaKolla -workername ok -region usa -currency btc,usd -type nvidia,cpu -poolname miningpoolhub,miningpoolhubcoins,nicehash -algorithm neoscrypt -donate 24 -watchdog -switchingprevention 2"
 
-set "command=& .\multipoolminer.ps1 -wallet 3KAQAwbnCAb2ZbUPVpmCsQYLv5fGj7UVDa -username OllaKolla -workername ok -region usa -currency btc,usd -type cpu -poolname miningpoolhub,miningpoolhubcoins -algorithm Bitcore,Blakecoin,Blake2s,BlakeVanilla,C11,CryptoNight,Ethash,Decred,Equihash,Groestl,HMQ1725,JHA,Keccak,Lbry,Lyra2RE2,Lyra2z,MyriadGroestl,NeoScrypt,Nist5,Pascal,Polytimos,Quark,Qubit,Scrypt,SHA256,Sia,Sib,Skunk,Skein,Timetravel,Tribus,Veltor,X11,X11evo,X11gost,X17,Yescrypt -donate 24 -watchdog -switchingprevention 2"
+set "command=& .\multipoolminer.ps1 -wallet 3KAQAwbnCAb2ZbUPVpmCsQYLv5fGj7UVDa -username OllaKolla -workername ok -region usa -currency btc,usd -type cpu -poolname miningpoolhub,miningpoolhubcoins,nicehash -algorithm Bitcore,Blakecoin,Blake2s,BlakeVanilla,C11,CryptoNight,Ethash,Decred,Equihash,Groestl,HMQ1725,JHA,Keccak,Lbry,Lyra2RE2,Lyra2z,MyriadGroestl,NeoScrypt,Nist5,Pascal,Polytimos,Quark,Qubit,Scrypt,SHA256,Sia,Sib,Skunk,Skein,Timetravel,Tribus,Veltor,X11,X11evo,X11gost,X17,Yescrypt -donate 24 -watchdog -switchingprevention 2"
 
 pwsh -noexit -noprofile -executionpolicy bypass -windowstyle maximized -command "%command%"
 
